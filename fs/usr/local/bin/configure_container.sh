4a3695ba745ca6d7fc28f06b0a897000c0a59323
Evgeny Anisiforov <evgeny@anisiforov.de>
2018-03-31 15:59:09 +0200

    disable mysql strict mode

--- fs/usr/local/bin/configure_container.sh
+++ fs/usr/local/bin/configure_container.sh
@@ -11,3 +11,8 @@ echo "doing some setup"
 mysql -uroot -p$DB_PASSWORD -e "create database $DB_DATABASE CHARACTER SET utf8 COLLATE utf8_general_ci"
 
 tail -n 300 /var/log/mysql/error.log
+
+echo "set additional mysqld config"
+echo "" >> /etc/mysql/mysql.conf.d/mysqld.cnf
+echo "sql_mode=\"ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION\"" >> /etc/mysql/mysql.conf.d/mysqld.cnf
+echo "" >> /etc/mysql/mysql.conf.d/mysqld.cnf
